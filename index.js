101e74a28b2b297c6e33410576ee46ae5405f2c0
vaninha049 <vaninha049@hotmail.com>
2024-07-08 19:49:30 -0300

    Codigo Pronto

--- index.js
+++ index.js
@@ -25,3 +25,36 @@ Se XP for maior ou igual a 10.001 = Radiante
 Ao final deve se exibir uma mensagem:
 "O Herói de nome **{nome}** está no nível de **{nivel}**"*/
 
+function determinarNivelHeroi() {
+    // Solicita o nome do herói e sua XP
+    let nomeHeroi = prompt("Digite o nome do herói:");
+    let xpHeroi = parseInt(prompt("Digite a quantidade de XP do herói:"));
+
+    let nivel;
+
+    // Determina o nível baseado na XP
+    if (xpHeroi < 1000) {
+        nivel = "Ferro";
+    } else if (xpHeroi >= 1001 && xpHeroi <= 2000) {
+        nivel = "Bronze";
+    } else if (xpHeroi >= 2001 && xpHeroi <= 5000) {
+        nivel = "Prata";
+    } else if (xpHeroi >= 5001 && xpHeroi <= 7000) {
+        nivel = "Ouro";
+    } else if (xpHeroi >= 7001 && xpHeroi <= 8000) {
+        nivel = "Platina";
+    } else if (xpHeroi >= 8001 && xpHeroi <= 9000) {
+        nivel = "Ascendente";
+    } else if (xpHeroi >= 9001 && xpHeroi <= 10000) {
+        nivel = "Imortal";
+    } else if (xpHeroi >= 10001) {
+        nivel = "Radiante";
+    }
+
+    // Exibe a mensagem com o nível do herói
+    console.log(`O Herói de nome ${nomeHeroi} está no nível de ${nivel}`);
+}
+
+// Chamada da função principal
+determinarNivelHeroi();
+
